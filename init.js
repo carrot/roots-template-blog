297962d5c2cf843834122337a7cef00bae9c5e8a
Jeff Escalante <jescalan@users.noreply.github.com>
2016-08-23 10:17:32 -0400

    convert to new sprout version

---
+++ init.js
@@ -0,0 +1,9 @@
+exports.configure = [{
+  type: 'input',
+  name: 'name',
+  message: 'What is the name of your project?'
+}, {
+  type: 'input',
+  name: 'description',
+  message: 'Describe your project'
+}]
